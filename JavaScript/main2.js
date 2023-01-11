456be5e052ad5259d62d113c5adaf2e09d3888a4
influencejr <davidvovk@proton.me>
2023-01-11 11:35:26 +0200

    Added another function

---
+++ JavaScript/main2.js
@@ -0,0 +1,7 @@
+
+function myFunc(){
+    let input = Number(prompt("Enter first number: ", 0))
+    let input2 = Number(prompt("Enter second number: ", 0))
+    let result = input + input2
+    document.write(`The result is: ${result}`)
+}
